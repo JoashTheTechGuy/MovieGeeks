fb80fbe1fc7757b22e3d039ea9c42f38ec0d6eb1
JoashTheTechGuy <bossmanpolite@gmail.com>
2025-04-19 23:03:18 -0400

    updated files (with firebase)

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1__gh0IFYjv6Ap4Hze4tLBR6IvJ7IUAs",
+    authDomain: "recs-4-u.firebaseapp.com",
+    projectId: "recs-4-u",
+    storageBucket: "recs-4-u.firebasestorage.app",
+    messagingSenderId: "341745953581",
+    appId: "1:341745953581:web:0ab297709b8695abac01a4",
+    measurementId: "G-V5GQRXWP1T"
+};
+
+export default firebaseConfig;
